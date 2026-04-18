44a983cb502b304dd445f34b584c5a2d9e9434a9
codejoncode <your-email@example.com>
2026-04-18 15:18:54 -0500

    production build

--- src/images.d.ts
+++ src/images.d.ts
@@ -1,3 +1,9 @@
+declare module "*.css";
+declare module "*.module.css" {
+  const classes: { readonly [key: string]: string };
+  export default classes;
+}
+
 declare module "*.png" {
   const value: string;
   export default value;
